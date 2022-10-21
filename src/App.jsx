b3aa028ba9370e717a8af1f7a1c418cd09e34742
VolodymyrPlastun <vplastun8@gmail.com>
2022-10-21 12:27:42 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ export const App = () => {
             token.set(tokenValue);
             dispatch(refresh(tokenValue));
         }
-    }, [tokenValue]);
+    }, [dispatch, tokenValue]);
 
     if (isLoading) return <Loader />;
 
